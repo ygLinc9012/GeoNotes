7dad5e749980b7345e37c4282cf40aa17c31c231
Derek Liu <htliu1992@gmail.com>
2017-01-05 03:55:33 -0500

    Add files via upload

---
+++ CotanLaplace_main.m
@@ -0,0 +1,34 @@
+clc; clear all; close all;
+[V,F] = readOBJ('bunny.obj');
+L = CotanLaplace(V, F);
+[~, vertAreaList, ~, ~] = ComAreaAndNormal(F,V);
+Mv = sparse(size(vertAreaList,1), size(vertAreaList,1)); % mass matrix
+for idx = 1:size(vertAreaList,1)
+    %Mv = diag(vertAreaList);
+    Mv(idx, idx) = vertAreaList(idx);
+end
+
+% Compute eigenvalue and eigenvectors
+numEigs = 6;
+[eVec, eVal] = eigs(L + 1e-6.*speye(size(L,1),size(L,1)), Mv, numEigs, 'sm');
+[~ ,i] = sort(diag(eVal)); % sort
+eVal = sum(eVal,2);
+eVal = eVal(i) - 1e-6;
+eVec = eVec(:,i);
+
+% Visualize
+figure(1)
+plot(eVal)
+title('eigenvalue')
+grid on
+
+figure(2)
+for i = 1:numEigs
+    subplot(2,ceil(numEigs/2),i)
+    trimesh(F,V(:,1),V(:,2),V(:,3),eVec(:,i),'FaceColor','interp','edgecolor','interp');
+    colormap jet
+    axis equal
+    axis off
+    title(strcat('eigenvector#',int2str(i)))
+end
+
